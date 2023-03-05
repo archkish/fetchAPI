98e621cf31202a44385790b63143baa1a782f503
arch <kisharchi@gmail.com>
2023-03-05 13:18:53 +0100

    first

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+function getPosts () {
+  fetch('https://jsonplaceholder.typicode.com/posts')  
+  .then(response => response.json())
+  .then(json => {
+
+    let block = document.createElement('div')
+    block.className = 'out'
+    document.body.appendChild(block)
+
+    json.map((value) => {
+      
+      let titleText = `${value.id} Title: ${value.title}`
+      let bodyText = `Body: ${value.body}`
+      let text = `${titleText} ${bodyText}`
+      console.log(text)
+
+      let wrapper = document.createElement('div')
+      let title = document.createElement('p');
+      let bodyDiv = document.createElement('div')
+
+      title.textContent = titleText;
+      bodyDiv.textContent = bodyText;
+
+      block.appendChild(wrapper)
+      wrapper.appendChild(title); 
+      wrapper.appendChild(bodyDiv)
+    })
+
+  })
+}
+
+
+let btn = document.querySelector('.btn')
+btn.addEventListener('click', getPosts)
